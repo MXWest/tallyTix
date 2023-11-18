eb1714329bb7d09824ac0ff5b25d305512d1e2cd
MXWest <mikexwest@gmail.com>
2023-11-17 20:49:59 -0500

    Update Fri Nov 17 20:49:57 EST 2023 [317]

--- rowRealSeatStatus.js
+++ rowRealSeatStatus.js
@@ -1,7 +1,7 @@
 /* Copy/ Paste 'rowSeatRealStatus' from each section into the appropriate variables here: */
 export const leftRowSeatRealStatus = [
     ['', '', '', '', '', '', 'K', 'X', 'X'],
-    ['', '', 'O', 'O', 'X', 'X', 'O', 'O', 'X'],
+    ['', '', 'X', 'X', 'X', 'X', 'O', 'O', 'X'],
     ['', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X'],
     ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
     ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
@@ -35,12 +35,12 @@ export const centerRowSeatRealStatus = [
     ['', 'X', 'X', 'X', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
     ['', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'O', 'O', 'O', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'X', 'X', 'O', 'X', 'X'],
     ['', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'O', 'X', 'X'],
     ['', 'X', 'X', 'X', 'X', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
     ['X', 'X', 'X', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'X', 'X', 'X'],
-    ['', 'X', 'X', 'O', 'O', 'O', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'X'],
+    ['', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'O', 'O', 'X', 'X'],
     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'X'],
     ['', 'X', 'X', 'X', 'X', 'X', 'O', 'O', 'O', 'O', 'X', 'O', 'X', 'X'],
     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'X'],
